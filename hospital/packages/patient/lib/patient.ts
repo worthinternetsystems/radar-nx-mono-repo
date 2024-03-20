f9b717209a71936fd306562c48431d44a204f960
Henry Penton <henry.penton@kerv.com>
2024-03-20 14:23:39 +0000

    convert patient to typescript and build

---
+++ hospital/packages/patient/lib/patient.ts
@@ -0,0 +1,3 @@
+export const patient = () => {
+  return "Hello from patient";
+};
---
+++ hospital/packages/scheduler/lib/scheduler.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = scheduler;
+
+function scheduler() {
+  return 'Hello from scheduler';
+}
